d31b78b8804332172164b6e37367c7db06709f2e
Ayush Nikhara <ayushnikhara2002@gmail.com>
2025-01-03 02:57:14 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -2,12 +2,6 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div>{/*
-      Heads up! 👋
-    
-      Plugins:
-        - @tailwindcss/forms
-    */}
     
     <section className="bg-white">
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
@@ -71,10 +65,6 @@ export default function Page() {
               <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
                Ai-Interview-Mocker 🦑
               </h1>
-    
-              <p className="mt-4 leading-relaxed text-gray-500">
-               hello world
-              </p>
 
             </div>
     
